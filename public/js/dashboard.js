11a3ec91bcdad48f1802cc82f3f7d3c7b29bb2fe
Johannes <johannesrehm89@googlemail.com>
2016-01-19 16:29:56 +0100

    Erste Version des Visitors-Charts

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,76 @@
+function showVisitors(){
+	var margin = {top: 10, right: 10, bottom: 100, left: 0},
+			width  = 960 - margin.left - margin.right,
+			height = 500 - margin.top - margin.bottom;
+
+	var parseDate = d3.time.format("%d-%b-%Y").parse;
+
+	var x = d3.time.scale().range([0, width]),
+			y = d3.scale.linear().range([height, 0]);
+
+	var xAxis = d3.svg.axis().scale(x).orient("bottom"),
+			yAxis = d3.svg.axis().scale(y).orient("left");
+
+	var brush = d3.svg.brush().on("brush", brushed);
+
+	var area = d3.svg.area()
+			.interpolate("monotone")
+			.x(function(d) { return x(d.date); })
+			.y0(height)
+			.y1(function(d) { return y(d.hits); });
+
+	var svg = d3.select("#visitors_container").append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom);
+
+	svg.append("defs").append("clipPath")
+			.attr("id", "clip")
+			.append("rect")
+			.attr("width", width)
+			.attr("height", height);
+
+	var focus = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	d3.json("api/data", function (data) {
+
+		data = data.visitors;
+		data.sort(comp);
+
+		data.forEach(function(d) {
+			d.date = parseDate(d.date);
+		});
+
+
+
+		x.domain(d3.extent(data.map(function(d) { return d.date; })));
+		y.domain([0, d3.max(data.map(function(d) { return d.hits; }))]);
+
+		focus.append("path")
+				.datum(data)
+				.attr("clip-path", "url(#clip)")
+				.attr("d", area);
+
+		focus.append("g")
+				.attr("class", "x axis")
+				.attr("transform", "translate(0," + height + ")")
+				.call(xAxis);
+
+		focus.append("g")
+				.attr("class", "y axis")
+				.call(yAxis);
+	});
+
+	function brushed() {
+		x.domain(brush.empty() ? x2.domain() : brush.extent());
+		focus.select("path").attr("d", area);
+		focus.select(".x.axis").call(xAxis);
+	}
+
+	function comp(a, b) {
+		return new Date(a.date).getTime() - new Date(b.date).getTime();
+	}
+}
+
 // Barchart für die RequestedFiles
 function showRequestedFiles(){
 
