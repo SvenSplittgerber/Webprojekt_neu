e731fcdf97d1783ffe4a02202e5b661dbb5ba233
Craizan <johannesrehm89@googlemail.com>
2016-01-20 15:36:25 +0100

    Fertigstellung Visitor-Chart

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,73 +1,187 @@
 function showVisitors(){
-	var margin = {top: 10, right: 10, bottom: 100, left: 0},
-			width  = 960 - margin.left - margin.right,
-			height = 500 - margin.top - margin.bottom;
+	// Set the dimensions of the canvas / graph
+	var margin = {top: 30, right: 40, bottom: 30, left: 50},
+		width = 800 - margin.left - margin.right,
+		height = 530 - margin.top - margin.bottom;
 
-	var parseDate = d3.time.format("%d-%b-%Y").parse;
+// Parse the date / time
+	var parseDate = d3.time.format("%d/%b/%Y").parse,
+		formatDate = d3.time.format("%d/%b"),
+		bisectDate = d3.bisector(function(d) { return d.date; }).left;
 
-	var x = d3.time.scale().range([0, width]),
-			y = d3.scale.linear().range([height, 0]);
+// Set the ranges
+	var x = d3.time.scale().range([0, width]);
+	var y = d3.scale.linear().range([height, 0]);
 
-	var xAxis = d3.svg.axis().scale(x).orient("bottom"),
-			yAxis = d3.svg.axis().scale(y).orient("left");
+// Define the axes
+	var xAxis = d3.svg.axis().scale(x)
+		.orient("bottom").ticks(10);
 
-	var brush = d3.svg.brush().on("brush", brushed);
+	var yAxis = d3.svg.axis().scale(y)
+		.orient("left").ticks(10);
 
-	var area = d3.svg.area()
-			.interpolate("monotone")
-			.x(function(d) { return x(d.date); })
-			.y0(height)
-			.y1(function(d) { return y(d.hits); });
+// Define the line
+	var valueline = d3.svg.line()
+		.x(function(d) { return x(d.date); })
+		.y(function(d) { return y(d.hits); });
 
-	var svg = d3.select("#visitors_container").append("svg")
-			.attr("width", width + margin.left + margin.right)
-			.attr("height", height + margin.top + margin.bottom);
+// Adds the svg canvas
+	var svg = d3.select("#visitors_container")
+		.append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform",
+			"translate(" + margin.left + "," + margin.top + ")");
 
-	svg.append("defs").append("clipPath")
-			.attr("id", "clip")
-			.append("rect")
-			.attr("width", width)
-			.attr("height", height);
+	var lineSvg = svg.append("g");
 
-	var focus = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	var focus = svg.append("g")
+		.style("display", "none");
 
-	d3.json("api/data", function (data) {
+// Get the data
+	d3.json("api/data", function(error, data) {
 
 		data = data.visitors;
 		data.sort(comp);
 
 		data.forEach(function(d) {
 			d.date = parseDate(d.date);
+			d.hits = +d.hits;
 		});
 
+		// Scale the range of the data
+		x.domain(d3.extent(data, function(d) { return d.date; }));
+		y.domain([0, d3.max(data, function(d) { return d.hits; })]);
 
+		// Add the valueline path.
+		lineSvg.append("path")
+			.attr("class", "line")
+			.attr("d", valueline(data));
 
-		x.domain(d3.extent(data.map(function(d) { return d.date; })));
-		y.domain([0, d3.max(data.map(function(d) { return d.hits; }))]);
+		// Add the X Axis
+		svg.append("g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(0," + height + ")")
+			.call(xAxis);
 
-		focus.append("path")
-				.datum(data)
-				.attr("clip-path", "url(#clip)")
-				.attr("d", area);
+		// Add the Y Axis
+		svg.append("g")
+			.attr("class", "y axis")
+			.call(yAxis);
 
-		focus.append("g")
-				.attr("class", "x axis")
-				.attr("transform", "translate(0," + height + ")")
-				.call(xAxis);
+		// append the x line
+		focus.append("line")
+			.attr("class", "x")
+			.style("stroke", "grey")
+			.style("stroke-dasharray", "3,3")
+			.attr("y1", 0)
+			.attr("y2", height);
 
-		focus.append("g")
-				.attr("class", "y axis")
-				.call(yAxis);
+		// append the y line
+		focus.append("line")
+			.attr("class", "y")
+			.style("stroke", "grey")
+			.style("stroke-dasharray", "3,3")
+			.attr("x1", width)
+			.attr("x2", width);
+
+		// append the circle at the intersection
+		focus.append("circle")
+			.attr("class", "y")
+			.style("fill", "none")
+			.style("stroke", "grey")
+			.attr("r", 4);
+
+		// place the value at the intersection
+		focus.append("text")
+			.attr("class", "y1")
+			.style("stroke", "white")
+			.style("stroke-width", "3.5px")
+			.style("opacity", 0.8)
+			.attr("dx", 8)
+			.attr("dy", "-.3em");
+		focus.append("text")
+			.attr("class", "y2")
+			.attr("dx", 8)
+			.attr("dy", "-.3em");
+
+		// place the date at the intersection
+		focus.append("text")
+			.attr("class", "y3")
+			.style("stroke", "white")
+			.style("stroke-width", "3.5px")
+			.style("opacity", 0.8)
+			.attr("dx", 8)
+			.attr("dy", "1em");
+		focus.append("text")
+			.attr("class", "y4")
+			.attr("dx", 8)
+			.attr("dy", "1em");
+
+		// append the rectangle to capture mouse
+		svg.append("rect")
+			.attr("width", width)
+			.attr("height", height)
+			.style("fill", "none")
+			.style("pointer-events", "all")
+			.on("mouseover", function() { focus.style("display", null); })
+			.on("mouseout", function() { focus.style("display", "none"); })
+			.on("mousemove", mousemove);
+
+		function mousemove() {
+			var x0 = x.invert(d3.mouse(this)[0]),
+				i = bisectDate(data, x0, 1),
+				d0 = data[i - 1],
+				d1 = data[i],
+				d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+
+			focus.select("circle.y")
+				.attr("transform",
+					"translate(" + x(d.date) + "," +
+					y(d.hits) + ")");
+
+			focus.select("text.y1")
+				.attr("transform",
+					"translate(" + x(d.date) + "," +
+					y(d.hits) + ")")
+				.text(d.hits + " visitors");
+
+			focus.select("text.y2")
+				.attr("transform",
+					"translate(" + x(d.date) + "," +
+					y(d.hits) + ")")
+				.text(d.hits + " visitors");
+
+			focus.select("text.y3")
+				.attr("transform",
+					"translate(" + x(d.date) + "," +
+					y(d.hits) + ")")
+				.text(formatDate(d.date));
+
+			focus.select("text.y4")
+				.attr("transform",
+					"translate(" + x(d.date) + "," +
+					y(d.hits) + ")")
+				.text(formatDate(d.date));
+
+			focus.select(".x")
+				.attr("transform",
+					"translate(" + x(d.date) + "," +
+					y(d.hits) + ")")
+				.attr("y2", height - y(d.hits));
+
+			focus.select(".y")
+				.attr("transform",
+					"translate(" + width * -1 + "," +
+					y(d.hits) + ")")
+				.attr("x2", width + width);
+		}
+
 	});
 
-	function brushed() {
-		x.domain(brush.empty() ? x2.domain() : brush.extent());
-		focus.select("path").attr("d", area);
-		focus.select(".x.axis").call(xAxis);
-	}
-
 	function comp(a, b) {
-		return new Date(a.date).getTime() - new Date(b.date).getTime();
+		return new Date(a.date) - new Date(b.date);
 	}
 }
 
