26660a86fd4a27a30c7666abacca3d4733eaa35b
Craizan <johannesrehm89@googlemail.com>
2016-01-25 09:45:22 +0100

    Kommentar-Änderung

--- app/schemes.js
+++ app/schemes.js
@@ -1,5 +1,5 @@
 /* Author: Johannes Rehm */
-/* Function to initialize the Mongoose schemes in /models */
+/* Function to initialize the Mongoose schemes in /schemes */
 exports.initialize = function(){
     require("fs").readdirSync(__dirname + "/schemes").forEach(function(file){
         require('./schemes/' + file)();
